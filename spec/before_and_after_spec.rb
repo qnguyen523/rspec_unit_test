3a5101f475f40e864c4ee00080e68205e17ba273
Quoc Nguyen <qnguyen523@gmail.com>
2023-05-10 18:15:55 +0700

    save

--- spec/before_and_after_spec.rb
+++ spec/before_and_after_spec.rb
@@ -1,3 +1,20 @@
 RSpec.describe 'before and after hooks' do
-  
+  before(:context) do
+    puts 'before context'
+  end
+  after(:context) do
+    puts 'after context'
+  end
+  before(:example) do
+    puts 'before example'
+  end
+  after(:example) do
+    puts 'after example'
+  end
+  it 'random' do
+    expect(5*4).to eq(20)
+  end
+  it 'another random' do
+    expect(3-2).to eq(1)
+  end
 end
