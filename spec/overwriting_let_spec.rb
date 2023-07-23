c85535ddc9a4dd2efb7230a0c8538e4409cf4eb5
Quoc Nguyen <qnguyen523@gmail.com>
2023-07-24 00:33:23 +0700

    save

--- spec/overwriting_let_spec.rb
+++ spec/overwriting_let_spec.rb
@@ -5,4 +5,13 @@ class ProgrammingLanguage
   end
 end
 RSpec.describe ProgrammingLanguage do
+  let(:language) { ProgarmmingLanguage.new('Python') }
+  it 'should store the name of the language' do
+    expect(language.name).to(eq('Python'))
+  end
+  context 'with no argument' do
+    it 'should default to Ruby as the name' do
+      expect(language.name).to(eq('Python'))
+    end
+  end
 end
