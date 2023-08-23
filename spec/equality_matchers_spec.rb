2042675d3d4549b0397870ef58b188862bf88e4c
Quoc Nguyen <qnguyen523@gmail.com>
2023-08-23 19:10:21 +0700

    equality matcher

---
+++ spec/equality_matchers_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe 'equality matchers' do
+  let(:a) {3.0}
+  let(:b) {3}
+  describe 'eq matcher' do
+    it 'test for value and ignore type' do
+      expect(a).to eq(3)
+      expect(b).to eq(3.0)
+      expect(a).to eq(b)
+    end
+  end
+  describe 'eql matcher' do
+    it 'test for value, including same type' do
+      expect(a).not_to eql(3)
+      expect(b).not_to eql(3.0)
+      expect(a).not_to eql(b)
+    end
+  end
+end
