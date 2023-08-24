04d29751312ede7333b317451dd24f5c937ffa61
Quoc Nguyen <qnguyen523@gmail.com>
2023-08-24 14:54:33 +0700

    be matcher

---
+++ spec/be_matchers_spec.rb
@@ -0,0 +1,19 @@
+# falsey values : false, nil
+# truthy values : everything else
+RSpec.describe 'be matcher' do
+  it 'can test for truthiness' do
+    expect(true).to be_truthy
+    expect(-1).to be_truthy
+    expect([]).to be_truthy
+    expect({}).to be_truthy
+  end
+  it 'can test for falsiness' do
+    expect(false).to be_falsey
+    expect(nil).to be_falsey
+  end
+  it 'can test for nil' do
+    expect(nil).to be_nil
+    my_hash = {a: 5}
+    expect(my_hash[:b]).to be_nil
+  end
+end
